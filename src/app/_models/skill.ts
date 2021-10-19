613c0668e17441cd237729f449e47bf873457779
Diyar Faraj <diyar.faraj@gmail.com>
2021-10-19 22:37:33 +0200

    still working on candidate card

--- src/app/_models/skill.ts
+++ src/app/_models/skill.ts
@@ -1,5 +1,6 @@
 export interface Skill {
   id: number;
   name: string;
+  devIconName: string;
   yearsOfExperience: number;
 }
--- src/app/candidates/candidate-list/candidate-list.component.ts
+++ src/app/candidates/candidate-list/candidate-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Candidate } from 'src/app/_models/candidate';
+import { Skill } from 'src/app/_models/skill';
 import { CandidatesService } from 'src/app/_services/candidates.service';
 
 @Component({
